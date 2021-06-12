c3569feb3393efa72595e78de53e8f574d735024
cvCode <haizhia1@163.com>
2021-06-12 15:05:50 +0800

    添加了springboot

---
+++ sprigboot/src/test/java/com/wlwl/sprigboot/SprigbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlwl.sprigboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
