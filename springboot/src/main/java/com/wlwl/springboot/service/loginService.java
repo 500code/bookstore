ea24cafc8a39c115473243b5f4842cab428b4beb
肥肥 <fb291835@163.com>
2021-06-13 18:20:22 +0800

    删除文件 springboot/src/main/java/com/wlwl/springboot/service/loginService.java

--- springboot/src/main/java/com/wlwl/springboot/service/loginService.java
+++
@@ -1,7 +0,0 @@
-package com.wlwl.springboot.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wlwl.springboot.entity.User;
-
-public interface loginService extends IService<User> {
-}
