cb1264bea0e00fe8610cab7f2f7da50ba3c1a712
肥肥 <fb291835@163.com>
2021-06-13 14:16:18 +0800

    update

---
+++ sprigboot/src/main/java/com/wlwl/sprigboot/service/loginService.java
@@ -0,0 +1,7 @@
+package com.wlwl.sprigboot.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wlwl.sprigboot.entity.User;
+
+public interface loginService extends IService<User> {
+}
